9f33ed083c8e4be2c06843cadba5055c87b254ce
KAUSIC M <76256336+kausicm30@users.noreply.github.com>
2021-05-04 17:10:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var port =3030;
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(__dirname));
+
+app.set('views','./views');
+app.set('view engine','ejs');
+app.use(express.json());
+
+const myRoutes = require("./controller/router/route");
+app.use(myRoutes);
+
+
+var server=app.listen(port,function()
+{
+    console.log(`Server listening the port ${port}`);
+});
